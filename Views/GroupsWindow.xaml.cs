0cb31cc1d2360d3d42e66ef7cb5a8cb4714897a0
engelhaftes <divinegirl1980@gmail.com>
2026-04-28 15:33:46 +0300

    Add files via upload

---
+++ Views/GroupsWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace LearnSmart.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для GroupsWindow.xaml
+    /// </summary>
+    public partial class GroupsWindow : UserControl
+    {
+        public GroupsWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
